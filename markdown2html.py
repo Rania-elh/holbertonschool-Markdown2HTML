e9dd12f4902846e970cb9b975d8608eceb6b8cf6
Rania-elh <rania1elhefnawy@gmail.com>
2025-12-12 13:26:38 +0100

    Update

--- markdown2html.py
+++ markdown2html.py
@@ -2,23 +2,25 @@
 """Markdown2HTML Task 0 - validate args and create output file."""
 
 import sys
-from pathlib import Path
+import os
 
 if __name__ == "__main__":
     # Vérifie qu'il y a 2 arguments
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
-        exit(1)
+        sys.exit(1)
 
     md_file = sys.argv[1]
     html_file = sys.argv[2]
 
     # Vérifie que le Markdown existe
-    if not Path(md_file).is_file():
+    if not os.path.isfile(md_file):
         sys.stderr.write(f"Missing {md_file}\n")
-        exit(1)
+        sys.exit(1)
 
     # Crée le fichier HTML vide
-    Path(html_file).write_text("", encoding="utf-8")
+    with open(html_file, 'w', encoding='utf-8') as f:
+        pass  # rien à écrire pour Task 0
 
-    exit(0)
+    # Exit avec succès
+    sys.exit(0)
