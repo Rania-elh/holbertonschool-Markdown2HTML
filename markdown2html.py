a718cf9b8d576a884826a10928cdc5c18172fe2a
Rania-elh <rania1elhefnawy@gmail.com>
2025-12-13 16:45:30 +0100

    Update

--- markdown2html.py
+++ markdown2html.py
@@ -2,6 +2,7 @@
 import sys
 import os
 
+# Task 0: vérifier les arguments et le fichier Markdown
 if len(sys.argv) < 3:
     print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     sys.exit(1)
@@ -10,4 +11,5 @@ if not os.path.isfile(sys.argv[1]):
     print(f"Missing {sys.argv[1]}", file=sys.stderr)
     sys.exit(1)
 
+# Tout est bon → rien afficher
 sys.exit(0)
